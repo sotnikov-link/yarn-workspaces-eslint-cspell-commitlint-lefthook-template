10e6b60f7dd3213c1e1ed5e21831196c76c7ffae
Valeriy Sotnikov <sotnikov.link@gmail.com>
2024-07-14 10:17:19 +0300

    chore(root): add rule scope-separates-with-comma-and-space to commitlint.config.ts

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -15,13 +15,45 @@ try {
   console.warn({ warning: error });
 }
 
+const scopeSeparator = /[,/]/gu;
+
 const Configuration: UserConfig = {
   extends: ['@commitlint/config-conventional'],
   ignores: [(commit) => commit.startsWith('Pull request #')],
   rules: {
     'scope-empty': [Level.Error, 'never'],
     'scope-enum': [Level.Error, 'always', ['root', ...packageDirectoryList]],
+    'scope-separates-with-comma-and-space': [Level.Error, 'always'],
   },
+  plugins: [
+    {
+      rules: {
+        'scope-separates-with-comma-and-space': async ({ scope }) => {
+          if (scope) {
+            const separateCounter = [...scope.matchAll(scopeSeparator)];
+
+            if (separateCounter.length > 0) {
+              for (const [index, part] of scope
+                .split(scopeSeparator)
+                .entries()) {
+                const isValid =
+                  index === 0 ? !part.startsWith(' ') : part.startsWith(' ');
+
+                if (isValid === false) {
+                  return [
+                    isValid,
+                    `Use comma and space ", " before scope "${part}"`,
+                  ];
+                }
+              }
+            }
+          }
+
+          return [true];
+        },
+      },
+    },
+  ],
 };
 
 export default Configuration;
